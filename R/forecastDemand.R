23c2fec1d19d56357b6eb61d0cbe21a58f92788d
Grant Coble-Neal <grantcn96@gmail.com>
2022-06-09 12:40:43 +0800

    getting more out of mstl()

--- R/forecastDemand.R
+++ R/forecastDemand.R
@@ -5,9 +5,9 @@
 library(dplyr)
 library(forecast)
 
-demand <- analytics_df[, "Operational.Demand..MW."]
+demand <- ts(analytics_df[, "Operational.Demand..MW."], frequency = 48 * 365, start = c(2018, 1))
 
-demand %>% forecast::mstl() %>% forecast::autoplot() # assess multiple seasonal patterns
+demand %>% forecast::mstl(lambda = 0.95, iterate = 100, s.window = "periodic", t.window = 48 * 365) %>% forecast::autoplot() # assess multiple seasonal patterns
 
 fit <- auto.arima(analytics_df[, "Operational.Demand..MW."],
                   xreg = cbind(four)) 
