5f2cff6cb6506512d97628c28dea56c9b439eb50
Grant Coble-Neal <grantcn96@gmail.com>
2022-06-15 12:58:34 +0800

    A series of minor corrections to scripts

--- R/createAnalyticalDataset.R
+++ R/createAnalyticalDataset.R
@@ -5,9 +5,15 @@
 library(tidyverse)
 
 analytics_df <- left_join( WEM_data, df.30 ) %>% 
-  select( Calendar.Year, Calendar.Month, Calendar.Day, Trading.Date, Interval.Number, Trading.Interval, Operational.Demand..MW., AirTemperature ) %>%
+  select( Calendar.Year, Calendar.Month, Calendar.Day, Trading.Date, Interval.Number, Trading.Interval, Operational.Demand..MW., AirTemperature ) 
+
+analytics_df <- analytics_df %>%
   mutate( cooling = pmax(analytics_df[,"AirTemperature"], 18),
-          heating = pmin(analytics_df[,"AirTemperature"], 18)) %>% 
-  left_join( PublicHolidays ) %>% 
+          heating = pmin(analytics_df[,"AirTemperature"], 18)) 
+
+analytics_df <- analytics_df %>% 
+  left_join( PublicHolidays ) 
+
+analytics_df <- analytics_df %>% 
   mutate( holiday = ifelse( is.na(analytics_df$Public.Holiday), 0,1 ) )
 
