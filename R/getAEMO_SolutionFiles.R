48f6f827581e4a2a83dcb9f885de2b3d59f4edf0
Grant Coble-Neal <grantcn96@gmail.com>
2024-01-09 13:30:01 +0800

    created a json data extractor process

--- R/getAEMO_SolutionFiles.R
+++ R/getAEMO_SolutionFiles.R
@@ -1,10 +1,12 @@
 #
 #
-# Objective: get AEMO solution file data
+# Objective: get AEMO solution file data and explore its structure
 # Author: Grant Coble-Neal
-# Dependencies: 
+# Dependencies: nil
 # Source: https://data.wa.aemo.com.au/public/market-data/wemde/dispatchSolution/weekAheadDispatchData/previous/
-# References: (1) https://cran.r-project.org/web/packages/tidyjson/vignettes/introduction-to-tidyjson.html
+# References: 
+# (1) https://cran.r-project.org/web/packages/tidyjson/vignettes/introduction-to-tidyjson.html
+# (2) https://themockup.blog/posts/2020-05-22-parsing-json-in-r-with-jsonlite/
 
 library(here)
 library(jsonlite)
@@ -13,49 +15,58 @@ library(tidyjson)
 library(tidyverse)
 
 
-url_solution_file <- "https://data.wa.aemo.com.au/public/market-data/wemde/dispatchSolution/weekAheadDispatchData/previous/DispatchSolutionWeekAhead_20230930.zip"
-download.file(url_solution_file, destfile = 'DispatchSolution.zip')
-unzip(zipfile = 'DispatchSolution.zip', exdir = here('data'))
+# url_solution_file <- "https://data.wa.aemo.com.au/public/market-data/wemde/dispatchSolution/weekAheadDispatchData/previous/DispatchSolutionWeekAhead_20230930.zip"
+#  download.file(url_solution_file, destfile = 'DispatchSolution.zip')
+# unzip(zipfile = 'DispatchSolution.zip', exdir = here('data'))
 solution_file <- jsonlite::fromJSON("data/ReferenceWeekAhead-DispatchSolution_202309290800.json")
 
-solution_file$data$solutionData$schedule[1] %>% 
-  as_tibble() -> solution
+# explore the data ----
 
-solution_file.0 <- flatten(solution_file)$solutionData
-head(solution_file.0)
+str(solution_file, max.level = 1) # inspect the first level
+solution_file[["data"]][["solutionData"]] %>% str(max.level = 1)
+solution_file[["data"]][["solutionData"]][["schedule"]] %>% str(max.level = 1)
+solution_file[["data"]][["solutionData"]][["schedule"]][[1]] %>% str(max.level = 1)
 
-solution_file.2 <- solution_file %>% tidyr::unnest(data)
+# Compile the data ----
+
+# get primary dispatch interval
+solution_file %>% purrr::pluck("data", "primaryDispatchInterval") -> primaryDispatchInterval
+string_length = nchar(primaryDispatchInterval)
+
+Date = str_sub(primaryDispatchInterval, 1, 10)
+Time = str_sub(primaryDispatchInterval, string_length-4, string_length)
+Date_Time = paste(Date, Time, sep = " ")
+
+# Create custom function to add date-time to each data frame
+add_date_time <- function(df = df.11){
+  df %>% mutate(
+    DateTime = ymd_hm(Date_Time)
+  ) %>% relocate(DateTime) -> df.0
+  return(df.0)
+}
+
+# Create a custom function to select the desired data frame
+get_data_frame <- function(df.name = "schedule"){
+  solution_file %>% purrr::pluck("data", "solutionData", df, 1) -> df.0
+  df.0 %>% add_date_time() -> df.1
+  
+  # check if the last column is a list
+  column.number = ncol(df.1)
+  if(is.list(df.1[column.number])){
+    # if the last column is a list then flatten the data frame
+    df.1 %>% unnest(all_of(column.number)) -> df.1
+  }
+  
+  return(df.1)
+}
+
+# Create a custom function to extract the desired data frames
+extract_data_frames <- function(my.list = c("schedule", "dispatchCaps", "constraints")){
+  list.0 <- map(my.list, get_data_frame)
+  df <- bind_rows(list.0)
+  return(df)
+}
+
+data_extract <- extract_data_frames()
 
 
-# get number of columns needed
-rgx_split <- "\\."
-n_cols_max <-
-  solution_file.1 %>%
-  pull(name) %>% 
-  str_split(rgx_split) %>% 
-  map_dbl(~length(.)) %>% 
-  max()
-n_cols_max
-
-# separate the columns
-nms_sep <- paste0("name", 1:n_cols_max)
-solution_file.2 <-
-  solution_file.1 %>% 
-  separate(name, into = nms_sep, sep = rgx_split, fill = "right")
-
-solution_file.2 %>% pivot_longer(
-  cols = c(name3:name5),
-  names_to = "col_name",
-  values_to="classes"
-) -> solution_file.3
-
-solution_file.3  %>%  select(
-  name2, classes, value
-) -> solution_file.4
-
-names(solution_file.4) <- c("Class", "Classes", "Value")
-
-solution_file.4 %>% pivot_wider(
-  names_from = Classes,
-  values_from = Value
-) -> solution_file.5
